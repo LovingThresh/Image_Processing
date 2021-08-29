80d134e965e7c6ce9f573582718768011d2c7921
liuye <csu1704liuye@163.com>
2021-08-29 19:01:51 +0800

    增加了attention模块，增加的module的pytorch写法

---
+++ attention.py
@@ -0,0 +1,21 @@
+from tensorflow.keras.layers import Dense, Lambda, Dot, Activation, Concatenate
+from tensorflow.keras.layers import Layer
+
+
+class Attention(Layer):
+    def __init__(self, units=128, **kwargs):
+        self.units = units
+        super().__init__(**kwargs)
+
+    def __call__(self, inputs):
+        hidden_states = inputs
+        hidden_size = int(hidden_states.shape[2])
+        score_first_part = Dense(hidden_size, use_bias=False, name='attention_score_vec')(hidden_states)
+        h_t = Lambda(lambda x: x[:, -1, :], output_shape=(hidden_size,), name='last_hidden_state')(hidden_states)
+        score = Dot(axes=[1, 2], name='attention_score')([h_t, score_first_part])
+        attention_weights = Activation('softmax', name='attention_weight')(score)
+        context_vector = Dot(axes=[1, 1], name='context_vector')([hidden_states, attention_weights])
+        pre_activation = Concatenate(name='attention_output')([context_vector, h_t])
+        attention_vector = Dense(self.units, use_bias=False, activation='tanh', name='attention_vector')(pre_activation)
+
+        return attention_vector
---
+++ module_pytorch.py
@@ -0,0 +1,67 @@
+import torch
+from torch import nn
+from torchsummary import summary
+
+
+# Get cpu or gpu device for training
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print("Using {} device".format(device))
+# Define model
+
+
+class ResNetGenerator(nn.Module):
+    def __init__(self, input_channels=3, output_channels=2, dim=64, n_downsamplings=2,
+                 n_blocks=9, image_size=227):
+        super(ResNetGenerator, self).__init__()
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+        self.dim = dim
+        self.n_downsamplings = n_downsamplings
+        self.n_blocks = n_blocks
+        self.image_size = image_size
+
+        self.main_1 = nn.Sequential(
+            nn.Conv2d(self.input_channels, 64, (7, 7), padding=(3, 3), padding_mode='reflect', bias=False),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(),
+
+            nn.Conv2d(64, 128, (3, 3), stride=(2, 2), padding=(1, 1), padding_mode='zeros', bias=False),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(),
+
+            nn.Conv2d(128, 256, (3, 3), stride=(2, 2), padding=(1, 1), padding_mode='zeros', bias=False),
+            nn.InstanceNorm2d(256),
+            nn.ReLU()
+        )
+
+        self.residual_block = nn.Sequential(
+            nn.InstanceNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, (3, 3), stride=(1, 1), padding=(1, 1), padding_mode='reflect', bias=False),
+            nn.InstanceNorm2d(256)
+        )
+        self.main_2 = nn.Sequential(
+            nn.ConvTranspose2d(256, 128, (3, 3), stride=(2, 2), padding=(1, 1), padding_mode='zeros', bias=False),
+            nn.InstanceNorm2d(128),
+            nn.ReLU(),
+            nn.ConvTranspose2d(128, 64, (3, 3), stride=(2, 2), padding=(1, 1), padding_mode='zeros', bias=False),
+            nn.InstanceNorm2d(64),
+            nn.ReLU(),
+            nn.Conv2d(64, self.output_channels, (5, 5), (1, 1), padding=(3, 3), padding_mode='reflect'),
+            nn.Tanh()
+        )
+
+    def forward(self, x):
+        x = self.main_1(x)
+        for _ in range(self.n_blocks):
+            h = x
+            x = self.residual_block(x)
+            x = torch.add(x, h)
+        output = self.main_2(x)
+
+        return output
+
+
+model = ResNetGenerator().to(device)
+print(model)
+summary(model, (3, 227, 227), 10)
